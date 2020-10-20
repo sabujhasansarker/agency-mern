c6ca23bd531f1aa0059b8cc11229230577e1b6e5
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-20 18:22:34 +0600

    qraphQL support

--- server/server.js
+++ server/server.js
@@ -1,3 +1,31 @@
 // Database
 const db = require("./config/db");
 db();
+
+// GraphQL
+const { ApolloServer } = require("apollo-server");
+
+// type define
+const gql = require("graphql-tag");
+const typeDefs = gql`
+   type Query {
+      sayHi: String!
+   }
+`;
+
+// Query data
+const resolvers = {
+   Query: {
+      sayHi: () => "Hello World",
+   },
+};
+
+const server = new ApolloServer({
+   typeDefs,
+   resolvers,
+});
+
+// port
+server
+   .listen({ port: process.env.PORT || 5000 })
+   .then((res) => console.log(`Server is running at ${res.url}`));
