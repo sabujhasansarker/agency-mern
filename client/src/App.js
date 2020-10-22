e8a41110f6440fce8f178cf9178f01dc4c7b64b9
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-23 00:09:19 +0600

    redux setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,41 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+
+const getOrders = gql`
+   {
+      getOrders {
+         id
+         name
+         service {
+            id
+            title
+            icon
+            admin {
+               id
+               email
+            }
+         }
+      }
+   }
+`;
 
 function App() {
+   const { loading, error, data } = useQuery(getOrders);
+
+   if (loading) return "Loading...";
+   console.log(data.getOrders);
    return (
-      <div className="App">
-         <header className="App-header">
-            <h1>Hello</h1>
-         </header>
-      </div>
+      <Provider store={store}>
+         <div className="App">
+            <header className="App-header">
+               <h1>Hello</h1>
+            </header>
+         </div>
+      </Provider>
    );
 }
 
