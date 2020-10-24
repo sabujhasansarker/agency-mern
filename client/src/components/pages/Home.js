804b24c7ae8b55ea9da5375908cdbc0ff88c8c3e
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-24 23:32:38 +0600

    client

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,10 +1,33 @@
 import React, { Fragment } from "react";
 import Banner from "../template/Banner";
 
+// images
+import slack from "../../images/slack.png";
+import google from "../../images/google.png";
+import uber from "../../images/uber.png";
+import netflix from "../../images/netflix.png";
+import airbnb from "../../images/airbnb.png";
+
 const Home = () => {
+   const clients = [
+      { name: "slack", image: slack, url: "#" },
+      { name: "google", image: google, url: "#" },
+      { name: "uber", image: uber, url: "#" },
+      { name: "netflix", image: netflix, url: "#" },
+      { name: "airbnb", image: airbnb, url: "#" },
+   ];
    return (
       <Fragment>
          <Banner />
+         <section className="client">
+            <div className="container">
+               {clients.map((client, index) => (
+                  <a href={client.url} key={index} className={client.name}>
+                     <img src={client.image} alt={client.name} />
+                  </a>
+               ))}
+            </div>
+         </section>
       </Fragment>
    );
 };
