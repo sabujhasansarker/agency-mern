ea71f6aa44e3b507b35a56a053cb9ffdf4a6f27d
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-26 22:25:28 +0600

    mail send ok

--- server/graphQL/resolver/mail.js
+++ server/graphQL/resolver/mail.js
@@ -7,7 +7,7 @@ module.exports = {
    Mutation: {
       async sendMail(_, { email, name, message }) {
          try {
-            var transporter = nodemailer.createTransport({
+            let transporter = nodemailer.createTransport({
                service: "gmail",
                port: 554,
                secure: false,
@@ -16,28 +16,37 @@ module.exports = {
                   pass: config.get("pass"),
                },
             });
-            // const url =
-            //   req.protocol + "://" + req.get("host") + "/playground/" + newUser.id;
-            var mailOptions = {
-               from: config.get("email"),
-               to: email,
-               subject: `Thanks ${name}`,
-               html: `
+            let mailOptions = [
+               {
+                  from: config.get("email"),
+                  to: email,
+                  subject: `Thanks ${name}`,
+                  html: `
                        <h1 class="site-header__title" data-lead-id="site-header-title">THANK YOU!</h1>
                        <i class="fa fa-check main-content__checkmark" id="checkmark"></i>
                        <p class="main-content__body" data-lead-id="main-content-body">Thanks a bunch for filling that out. It means a lot to us, just like you do! We really appreciate you giving us a moment of your time today. Thanks for being you.</p>
                        <p class="site-footer__fineprint" id="fineprint">Copyright ©2014 | All Rights Reserved</p>`,
-            };
+               },
+               // Sent me
+               {
+                  from: config.get("email"),
+                  to: config.get("email"),
+                  subject: `Email sent from ${email}`,
+                  html: `${message}`,
+               },
+            ];
 
-            transporter.sendMail(mailOptions, function (error, info) {
-               if (error) {
-                  console.log(error);
-                  return "Error";
-               } else {
-                  console.log("Email sent: " + info.response);
+            mailOptions.map((mailO) => {
+               transporter.sendMail(mailO, function (error, info) {
+                  if (error) {
+                     console.log(error);
+                     return "Error";
+                  } else {
+                     console.log("Email sent: " + info.response);
 
-                  return "Ok";
-               }
+                     return "Ok";
+                  }
+               });
             });
          } catch (err) {
             throw new GraphQLError(err.message);
